764a4abd4068226cc7a47e57c06db2cb55d7b722
yams7457 <68620179+yams7457@users.noreply.github.com>
2021-09-19 20:07:48 -0500

    Update colorwheel002.lua

--- colorwheel002.lua
+++ colorwheel002.lua
@@ -1,5 +1,8 @@
 music_util = require("musicutil")
+
 m = midi.connect()
+
+include 'krit004'
 
   params:add_group("meta",18)
 
@@ -15,21 +18,26 @@ params:add{ type = "number", id = "midi channel " ..i, name = "midi channel " ..
 params:add{ type = "number", id = "track active " ..i, name = "track active " ..i, min = 0, max = 1, default = 1 }
 end
 
-  params:add_group("tracks",44)
+  params:add_group("tracks",56)
 
 for i = 1,4,1
   do
 params:add{ type = "number", id = "midi channel " ..i, name = "midi channel " ..i, min = 1, max = 16, default = 1 }
 params:add{ type = "number", id = "track active " ..i, name = "track active " ..i, min = 0, max = 1, default = 1 }
-params:add{ type = "number", id = "track octave " ..i, name = "track octave " ..i, min = 0, max = 5, default = i - 1 }
-params:add{ type = "number", id = "offset " ..i, name = "offset " ..i, min = 1, max = 5, default = math.random(1, 4)}
+params:add{ type = "number", id = "track octave " ..i, name = "track octave " ..i, min = 0, max = 5, default = i + 2 }
+params:add{ type = "number", id = "offset " ..i, name = "offset " ..i, min = 1, max = 5, default = math.random(2, 4)}
 params:add{ type = "number", id = "transposition " ..i, name = "transposition " ..i, min = -2, max = 2, default = math.random(-2, 2)}
-params:add{ type = "number", id = "carving " ..i, name = "carving " ..i, min = 0, max = 3, default = math.random (2,3) }
+params:add{ type = "number", id = "carving " ..i, name = "carving " ..i, min = 0, max = 3, default = math.random (0,3) }
 params:add{ type = "number", id = "probabilities " ..i, name = "probabilities " ..i, min = 1, max = 5, default = 1 }
 params:add{ type = "number", id = "clock channel " ..i, name = "clock channel " ..i, min = 1, max = 4, default = 1 }
-params:add{ type = "number", id = "gate sequence length " ..i, name = "gate sequence length " ..i, min = 1, max = 16, default = 12 + i}
-params:add{ type = "number", id = "interval sequence length " ..i, name = "interval sequence length " ..i, min = 1, max = 16, default = 11 + i}
-params:add{ type = "number", id = "octave sequence length " ..i, name = "octave sequence length " ..i, min = 1, max = 16, default = math.random (1, 16) }
+params:add{ type = "number", id = "gate sequence start " ..i, name = "gate sequence start " ..i, min = 1, max = 16, default = math.random(1, 6)}
+params:add{ type = "number", id = "interval sequence start " ..i, name = "interval sequence start " ..i, min = 1, max = 16, default = math.random(1, 6)}
+params:add{ type = "number", id = "octave sequence start " ..i, name = "octave sequence start " ..i, min = 1, max = 16, default = math.random (1, 6) }
+params:add{ type = "number", id = "gate sequence end " ..i, name = "gate sequence end " ..i, min = 1, max = 16, default = math.random(9, 16)}
+params:add{ type = "number", id = "interval sequence end " ..i, name = "interval sequence end " ..i, min = 1, max = 16, default = math.random(9, 16)}
+params:add{ type = "number", id = "octave sequence end " ..i, name = "octave sequence end " ..i, min = 1, max = 16, default = math.random (9, 16) }
+
+
 end
 
   params:add_group("steps",192)
@@ -40,7 +48,7 @@ for i = 1,4,1 do
 
       params:add{ type = "number", id = "gate " ..i .." "..j, name = "gate " ..i .." "..j, min = 0, max = 1, default = math.random(0, 1)}
       params:add{ type = "number", id = "interval " ..i .." "..j, name = "interval " ..i .." "..j, min = 1, max = 7, default = math.random (1, 7) }
-      params:add{ type = "number", id = "octave " ..i .." "..j, name = "octave " ..i .." "..j, min = 1, max = 7, default = 1 }
+      params:add{ type = "number", id = "octave " ..i .." "..j, name = "octave " ..i .." "..j, min = 0, max = 6, default = 0 }
 
     end
 
@@ -75,26 +83,29 @@ current_channel = {}
 
 function init()
   clock_id = clock.run(tick)
+  params:set("clock_tempo",math.random(30, 200))
   m = midi.connect()
   for i = 1,4,1 do
   current_gate_step[i] = 0
   current_interval_step[i] = 0
   current_octave_step[i] = 0
+  m:all_off()
+  krit004.init()
 end
 end
 
 function tick()
   while true do
-  clock.sync(1/8)
+  clock.sync(1/2)
   step = step + 1
   local current_inner_index = {}
   local current_octave = {}
   local current_offset = {}
   for i=1,4,1 do
-  current_gate_step[i] = (current_gate_step[i]) % params:get("gate sequence length " ..i) + 1
-  current_interval_step[i] = (current_interval_step[i] ) % params:get("interval sequence length " ..i) + 1
-  current_octave_step[i] = (current_octave_step[i] ) % params:get("octave sequence length " ..i)  + 1
-if current_gate_step[i] then
+  current_gate_step[i] = (((current_gate_step[i]) % (params:get("gate sequence end " ..i))) - (params:get("gate sequence start " ..i))) 
+  + 1 + (params:get("gate sequence start " ..i))
+  current_interval_step[i] = (((current_interval_step[i]) % (params:get("interval sequence end " ..i))) - (params:get("interval sequence start " ..i))) + 1 + (params:get("interval sequence start " ..i))
+  current_octave_step[i] = (((current_octave_step[i]) % (params:get("octave sequence end " ..i))) - (params:get("octave sequence start " ..i))) + 1 + (params:get("octave sequence start " ..i))
     local outer_index = 1 + params:get("transpose")
     local carving = params:get("carving " ..i) * 7
     local interval = params:get("interval " .. i .. " " .. current_interval_step[i])
@@ -105,8 +116,8 @@ if current_gate_step[i] then
     current_offset[i] = offset_list[params:get("offset mode")][params:get("offset " ..i)]
     current_note[i] = current_interval[i] + current_octave[i] + current_offset[i]
     current_channel[i] = params:get("midi channel " ..i)
-    play(current_note[i], 127, current_channel[i], i)
-    end
+    play(current_note[i], math.random(60, 127) - current_note[i], current_channel[i], 1)
+
   end
 end
 end
@@ -116,8 +127,18 @@ function play(note, vel, channel, track)
   m:note_on(note, vel, channel)
   clock.run(
     function()
-      clock.sleep(1/16)
+      clock.sleep(1)
           m:note_off(note, vel, channel)
 end
 )
+end
+
+function m:all_off()
+  for note = 1, 127 do
+    for channel = 1, 16 do
+      for device = 1, 4 do
+        m:note_off(note, 0, channel)
+      end
+    end
+  end
 end
